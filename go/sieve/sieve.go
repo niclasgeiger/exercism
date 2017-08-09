34148b9b41412dacb0007281c1e5e0d3bb7bf968
Niclas Geiger <niclas.geiger.extern@sixt.om>
2017-08-09 07:49:32 +0200

    files

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,1 +1,23 @@
 package sieve
+
+const testVersion = 1
+
+func Sieve(limit int) []int {
+	marked := make(map[int]bool, limit+1)
+	marked[0] = true
+	marked[1] = true
+	for i := 2; i <= limit; i++ {
+		if !marked[i] {
+			for j := 2 * i; j <= limit; j += i {
+				marked[j] = true
+			}
+		}
+	}
+	out := []int{}
+	for i := 2; i <= limit; i++ {
+		if !marked[i] {
+			out = append(out, i)
+		}
+	}
+	return out
+}
